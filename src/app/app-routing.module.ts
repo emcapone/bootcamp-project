2e3c2a29b34f924f4f739bf3b1ca0c4733cafcfd
Emily Foglia <efoglia@ceiamerica.com>
2022-07-05 12:07:53 -0400

    adding account profile, mock pet data, and css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountProfileComponent } from './account-profile/account-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'account-profile', component: AccountProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
