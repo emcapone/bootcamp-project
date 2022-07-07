36557a8d96e641afa0f2c345bb330931b14a8119
Emily Foglia <efoglia@ceiamerica.com>
2022-07-07 12:57:35 -0400

    changes to interface and mock data

--- src/app/mock-pets.ts
+++ src/app/mock-pets.ts
@@ -4,29 +4,65 @@ export const PETS: Pet[] = [
     {
         id: 1,
         name: 'Cassidy',
-        birthday: '11-12-2019',
+        breed: 'Domestic Shorthair',
+        sex: 'Male',
+        birthday: new Date('11/12/2019'),
         microchip: '7245565187654',
-        upcoming: 'Vet Visit (7/14/22)'
+        upcoming: 'Vet Visit (7/14/22)',
+        vaccines: [{
+            id: 1,
+            name: 'Rabies',
+            date: new Date('12/16/2020'),
+            due: new Date('12/16/2021')
+        }],
+        imageUrl: '/assets/cat.jpg'
     },
     {
         id: 2,
         name: 'Hallie',
-        birthday: '3-28-2021',
+        breed: 'Domestic Shorthair',
+        sex: 'Female',
+        birthday: new Date('3/28/2021'),
         microchip: '1456123456321',
-        upcoming: 'None (Add Event?)'
+        upcoming: 'None (Add Event?)',
+        vaccines: [{
+            id: 1,
+            name: 'Rabies',
+            date: new Date('12/16/2020'),
+            due: new Date('12/16/2021')
+        }],
+        imageUrl: '/assets/cat.jpg'
     },
     {
         id: 3,
         name: 'Megan',
-        birthday: '12-02-2017',
+        breed: 'Domestic Shorthair',
+        sex: 'Female',
+        birthday: new Date('12/02/2017'),
         microchip: '984531548632',
-        upcoming: 'Rabies Vaccine Due (1/20/23)'
+        upcoming: 'Rabies Vaccine Due (1/20/23)',
+        vaccines: [{
+            id: 1,
+            name: 'Rabies',
+            date: new Date('12/16/2020'),
+            due: new Date('12/16/2021')
+        }],
+        imageUrl: '/assets/cat.jpg'
     },
     {
         id: 4,
         name: 'Ezra',
-        birthday: '6-17-2010',
+        breed: 'Domestic Shorthair',
+        sex: 'Male',
+        birthday: new Date('6/17/2010'),
         microchip: 'None (Add Microchip?)',
-        upcoming: 'Vet Visit (8/10/22)'
+        upcoming: 'Vet Visit (8/10/22)',
+        vaccines: [{
+            id: 1,
+            name: 'Rabies',
+            date: new Date('12/16/2020'),
+            due: new Date('12/16/2021')
+        }],
+        imageUrl: '/assets/cat.jpg'
     }
 ]
--- src/app/pet.ts
+++ src/app/pet.ts
@@ -1,7 +1,19 @@
 export interface Pet {
-    id: number;
-    name: string;
-    birthday: string;
-    microchip: string;
-    upcoming: string;
+  id: number;
+  name: string;
+  breed: string;
+  sex: string;
+  birthday: Date;
+  microchip: string;
+  upcoming: string;
+  vaccines: Vaccination[]
+  imageUrl: string;
+}
+
+export interface Vaccination {
+  id: number;
+  name: string;
+  date: Date;
+  dose?: number;
+  due?: Date;
 }
