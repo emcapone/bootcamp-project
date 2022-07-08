832b0a4e32a37961072028d697679b10782970f9
Emily Foglia <efoglia@ceiamerica.com>
2022-07-08 14:39:30 -0400

    add 404 component, add nav bar component, rename routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountPetComponent } from './account-pet/account-pet.component';
 import { AccountProfileComponent } from './account-profile/account-profile.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
-  { path: 'account-profile', component: AccountProfileComponent },
-  { path: 'account-pet', component: AccountPetComponent }
+  { path: 'profile', component: AccountProfileComponent },
+  { path: 'new-pet', component: AccountPetComponent },
+  {path: '404', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/404'}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccountProfileComponent } from './account-profile/account-profile.component';
 import { AccountPetComponent } from './account-pet/account-pet.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AccountProfileComponent,
-    AccountPetComponent
+    AccountPetComponent,
+    NavigationBarComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
