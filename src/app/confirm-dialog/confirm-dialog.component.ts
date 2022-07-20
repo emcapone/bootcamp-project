ee29a0374caec18946df32a610280a3d39226b89
Emily Foglia <efoglia@ceiamerica.com>
2022-07-20 15:35:29 -0400

    add dialog for form cancel and pet deletion

---
+++ src/app/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,27 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+
+@Component({
+  selector: 'app-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+  styleUrls: ['./confirm-dialog.component.css']
+})
+export class ConfirmDialogComponent implements OnInit {
+
+  constructor(private dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: {
+      title: string,
+      message: string
+    }) { }
+
+  ngOnInit(): void { }
+
+  yes() {
+    this.dialogRef.close(true);
+  }
+  no() {
+    this.dialogRef.close(false);
+  }
+
+}
--- src/app/view-pet/view-pet.component.ts
+++ src/app/view-pet/view-pet.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { take } from 'rxjs';
 
 import { PetService } from '../pet.service';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-view-pet',
@@ -14,7 +16,7 @@ export class ViewPetComponent implements OnInit {
   pet$ = this.petService.pet$;
   displayVetRecords: boolean = false;
 
-  constructor(private router: Router, private petService: PetService, private route: ActivatedRoute) { }
+  constructor(private dialog: MatDialog, private router: Router, private petService: PetService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.petService.selectedPetChanged(Number(this.route.snapshot.paramMap.get('id')));
@@ -33,5 +35,22 @@ export class ViewPetComponent implements OnInit {
 
   toggle() {
     this.displayVetRecords = !this.displayVetRecords
+  }
+
+  openDialog(name: string) {
+    let dialog = this.dialog.open(ConfirmDialogComponent, {
+      width: '50%',
+      data: {
+        title: 'Delete Profile: ' + name,
+        message: 'This action cannot be undone. Are you sure?'
+      }
+    });
+    dialog.afterClosed().pipe(
+      take(1)
+    ).subscribe(res => {
+      if(res){
+        this.delete();
+      }
+    });
   }
 }
