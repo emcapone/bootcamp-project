782628c2fee54464e34de3e58a15d9d1a5a166f5
Emily Foglia <efoglia@ceiamerica.com>
2022-08-16 15:47:28 -0400

    Test time format pipe

--- src/app/shared/pipes/time-format.pipe.spec.ts
+++ src/app/shared/pipes/time-format.pipe.spec.ts
@@ -1,8 +1,55 @@
 import { TimeFormatPipe } from './time-format.pipe';
 
 describe('TimeFormatPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TimeFormatPipe();
+  let pipe: TimeFormatPipe;
+
+  beforeEach(async () => {
+    pipe = new TimeFormatPipe();
+  });
+
+  it('should create an instance', () => {
     expect(pipe).toBeTruthy();
   });
+
+  it('should return an empty string if value is undefined', () => {
+
+    expect(pipe.transform(undefined)).toEqual('');
+
+  });
+
+  it('should throw an error if time has only letters for hours and/or minutes', () => {
+
+    expect(() => { pipe.transform('as:fg') }).toThrow(new Error('Hours and minutes must not be letters.'));
+
+  });
+
+  it('should throw an error if the string is not in time format (00:00)', () => {
+
+    expect(() => { pipe.transform('1245') }).toThrow(new Error('Time must be in 24-hr format: 00:00'));
+
+  });
+
+  it('should throw an error if minutes is not 0-59', () => {
+
+    expect(() => { pipe.transform('12:78') }).toThrow(new Error('Minutes must be 0-59.'));
+
+  });
+  it('should throw an error if hours is not 0-23', () => {
+
+    expect(() => { pipe.transform('25:30') }).toThrow(new Error('Hours must be 0-23.'));
+
+  });
+
+  it('should return 12:00 AM if time is 00:00', () => {
+
+    expect(pipe.transform('00:00')).toEqual('12:00 AM');
+
+  });
+
+  it('should return 11:15 PM if time is 23:15', () => {
+
+    expect(pipe.transform('23:15')).toEqual('11:15 PM');
+
+  });
+
 });
--- src/app/shared/pipes/time-format.pipe.ts
+++ src/app/shared/pipes/time-format.pipe.ts
@@ -5,34 +5,53 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimeFormatPipe implements PipeTransform {
 
-  transform(value: String | undefined): string {
-    if(!value) {
-      return '';
-    }
-    let str = "";
-    let hours = parseInt(value.slice(0, 2));
-    let minutes = parseInt(value.slice(3));
+  transform(value: string | undefined): string {
+    try {
+      if (!value) {
+        return '';
+      }
 
-    let indicator = 'PM';
-    if(hours < 12) {
-      indicator = 'AM';
-    }
-    if (hours >= 13) {
-      hours -= 12;
-    }
-    if (hours === 0) {
-      hours = 12;
-    }
-    str += hours + ":";
+      let hours = parseInt(value.slice(0, 2));
+      let minutes = parseInt(value.slice(3));
+      let str = "";
+
+      if (isNaN(hours) || isNaN(minutes)) {
+        throw new Error('Hours and minutes must not be letters.');
+      }
+      if (value.charAt(2) !== ":") {
+        throw new Error('Time must be in 24-hr format: 00:00');
+      }
+      if (hours < 0 || hours > 23) {
+        throw new Error('Hours must be 0-23.');
+      }
+      if (minutes < 0 || minutes > 59) {
+        throw new Error('Minutes must be 0-59.');
+      }
+
+      let indicator = 'PM';
+      if (hours < 12) {
+        indicator = 'AM';
+      }
+      if (hours >= 13) {
+        hours -= 12;
+      }
+      if (hours === 0) {
+        hours = 12;
+      }
+      str += hours + ":";
 
 
-    if(minutes < 10){
-      str += "0";
-    }
-    str += minutes + " ";
-    str += indicator;
+      if (minutes < 10) {
+        str += "0";
+      }
+      str += minutes + " ";
+      str += indicator;
 
-    return str;
+      return str;
+    } catch (e) {
+      throw e;
+    }
+
   }
 
 }
