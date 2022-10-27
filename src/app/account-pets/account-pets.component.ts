68bd1a5bf08efe508628f9a1bd8cabe1fee0cbc8
Emily Foglia <efoglia@ceiamerica.com>
2022-10-27 15:56:54 -0400

    remove resolver so PetListItems updates correctly

--- src/app/account-pets/account-pets.component.ts
+++ src/app/account-pets/account-pets.component.ts
@@ -1,22 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { PetListItem } from '../pet-list-item';
+import { PetService } from '../pet.service';
 
 @Component({
   selector: 'app-account-pets',
   templateUrl: './account-pets.component.html',
   styleUrls: ['./account-pets.component.css']
 })
-export class AccountPetsComponent implements OnInit{
+export class AccountPetsComponent implements OnInit, OnDestroy{
 
   apiUrl = environment.apiUrl + '/';
   pets!: PetListItem[];
+  sub!: Subscription;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private petService: PetService) { }
 
   ngOnInit() {
-    this.pets = this.activatedRoute.snapshot.data['pets'];
+    this.sub = this.petService.pets$.subscribe(pets => this.pets = pets);
   }
 
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
